360a1f30495768d120afbf9725103ec1dffcc745
Jernej Kos <k@jst.sm>
2012-04-10 15:00:08 +0200

    Implemented VPN interface detection.

--- util/nodewatcher-core/files/modules/vpn.sh
+++ util/nodewatcher-core/files/modules/vpn.sh
@@ -8,11 +8,30 @@ MODULE_ID="core.vpn"
 MODULE_SERIAL=1
 
 #
+# VPN device name detection
+#
+check_vpn_device()
+{
+  local test_devices="$@"
+  for test_device in $test_devices; do
+    ip link sh dev ${test_device} >/dev/null 2>/dev/null
+    if [ "$?" == "0" ]; then
+      VPN_DEVICE="${test_device}"
+      return
+    fi
+  done
+}
+
+#
 # Report output function
 #
 report()
 {
-  show_entry "net.vpn.upload_limit" "`tc qdisc show dev tap0 2>/dev/null | grep -Eo '[0-9]+.?bit'`"
-  show_entry "net.vpn.mac" "`ip link show dev tap0 2>/dev/null | tail -n 1 | awk '{ print $2 }'`"
+  # Detect the correct VPN device
+  check_vpn_device tap0 tap1 edge0 edge1
+
+  # Output VPN information
+  show_entry "net.vpn.upload_limit" "`tc qdisc show dev ${VPN_DEVICE} 2>/dev/null | grep -Eo '[0-9]+.?bit'`"
+  show_entry "net.vpn.mac" "`ip link show dev ${VPN_DEVICE} 2>/dev/null | tail -n 1 | awk '{ print $2 }'`"
 }
 
